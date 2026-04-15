c2fdac3279a7ed8fbca4464bb8edb9d7712673e4
Cristian Ribeiro Etchebest <cristian.etchebest@gmail.com>
2026-04-15 10:11:59 -0300

    wip:add-module-components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ButtonComponent } from './components/button/button.component';
-import { CardComponent } from './components/card/card.component';
+import { ComponentsModule } from './components/components.module';
 
 @NgModule({
-  declarations: [AppComponent, ButtonComponent, ButtonComponent, CardComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent],
+  imports: [BrowserModule, ComponentsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
